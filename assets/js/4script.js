939112d03e6f43df0a2711525141a24562df487d
JOHAN <johangonzalez81@gmail.com>
2023-06-21 09:11:35 -0400

    mejora validador

--- assets/js/4script.js
+++ assets/js/4script.js
@@ -4,11 +4,11 @@ let color
 document.addEventListener('keydown', function (event) {
     eleme1= document.querySelector("#mensaje").innerHTML="Elija una opcion para cambiar el color del cuadro"
 
-    if (event.key === 'a') {
+    if (event.key === 'a' || event.key === 'A') {
     color = "blue"
-    }else if (event.key === 's') {
+    }else if (event.key === 's' || event.key === 'S') {
     color = "white"
-    }else if (event.key === 'd') {
+    }else if (event.key === 'd' || event.key === 'D') {
     color = "black"
     }else{
         eleme2= document.querySelector("#mensaje").innerHTML="Introzca solo la opcion indicada"
